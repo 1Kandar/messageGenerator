23478fc7db943382145643cad93ff7378862dbc7
kandar <1299mamiz1453@gmail.com>
2023-05-20 07:02:39 +0300

    Last version of the project

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const messages=["To live is the rarest thing in the world. Most people exist, that is all.","Be yourself; everyone else is already taken.","True friends stab you in the front.", "Women are made to be Loved, not understood.","Be the change that you wish to see in the world.","Live as if you were to die tomorrow. Learn as if you were to live forever.","No one can make you feel inferior without your consent.","Great minds discuss ideas; average minds discuss events; small minds discuss people.","Do what you feel in your heart to be right - for you'll be criticized anyway.","Do one thing every day that scares you.","Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate; only love can do that.","Our lives begin to end the day we become silent about things that matter.","In the end, we will remember not the words of our enemies, but the silence of our friends.","Injustice anywhere is a threat to justice everywhere.", "The time is always right to do what is right.","Life's most persistent and urgent question is, 'What are you doing for others?","Weak people revenge. Strong people forgive. Intelligent People Ignore.","I have not failed. I've just found 10,000 ways that won't work.","Genius is one percent inspiration and ninety-nine percent perspiration.","Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time."]
 
-let numberDedicator=Math.ceil(Math.random()*messages.length)
+
 
 
 document.querySelector("button").addEventListener("click",clicked)
@@ -11,13 +11,10 @@ const messageSelector= num =>{
     
 }
 
-const randomNumber=()=>{
-    return Math.floor(Math.random()*messages.length)
-}
 
 function clicked(){
-    let numberDedicator=randomNumber()
+  
     
-    document.querySelector("#message").innerHTML=messageSelector(numberDedicator)
+    document.querySelector("#message").innerHTML=messageSelector(Math.floor(Math.random()*messages.length))
 }
 
